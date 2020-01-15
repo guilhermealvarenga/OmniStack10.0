8ebaee807ff1856eb422988ba21f971e530d487c
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-01-14 22:32:33 -0300

    Backend do projeto, configurações iniciais

---
+++ src/backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-2ajzh.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+// Tipos de parâmetros:
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+// Mongoose dá acesso ao Node (aplicação) há uma base de dados MongoDB
+
+app.listen(3333);
---
+++ src/backend/src/routes.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+
+const routes = Router();
+
+routes.post('/devs', (request, response) => {
+  console.log(request.body);
+  return response.json({ message: 'Hello OmniStack'});
+});
+
+module.exports = routes;
