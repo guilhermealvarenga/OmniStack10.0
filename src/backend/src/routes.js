25b1ab4c2e1b557c922cd87f9e52a7b794c5715b
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-01-14 23:46:45 -0300

    Criando a base da aplicação

--- src/backend/src/routes.js
+++ src/backend/src/routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
-routes.post('/devs', (request, response) => {
-  console.log(request.body);
-  return response.json({ message: 'Hello OmniStack'});
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
