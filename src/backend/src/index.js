1f7fe06888ca692fc5123183f2a6cf37fabde41a
Guilherme de Alvarenga <gui.alva50@gmail.com>
2020-01-15 22:50:40 -0300

    Aplicação Web

--- src/backend/src/index.js
+++ src/backend/src/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
-// mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-2ajzh.mongodb.net/week10?retryWrites=true&w=majority', {
-mongoose.connect('mongodb://localhost:27017/week10', {
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-2ajzh.mongodb.net/week10?retryWrites=true&w=majority', {
+// mongoose.connect('mongodb://localhost:27017/week10', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
